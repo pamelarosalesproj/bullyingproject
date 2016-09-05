cfd508a65db8915a028abd517a3f41cfcfd76210
Pamela RS <>
2016-09-05 13:47:23 -0300

    1 - Criando o projeto no GitHub

---
+++ src/main/java/edu/bullyingmestrado/commons/Constants.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.bullyingmestrado.commons;
+
+/**
+ *
+ * @author pamela
+ */
+public final class Constants {
+	
+	  public static final String EMPTY_STRING = "";
+	  public static final String SPACE = " ";
+	  public static final String TAB = "\t";
+	  public static final String SINGLE_QUOTE = "'";
+	  public static final String PERIOD = ".";
+	  public static final String DOUBLE_QUOTE = "\"";
+	  
+	  
+	  /*
+	   * Types of Classification for Severity Tool
+	   * */
+	  public static final String CLA_TRACE= "trace";
+	  public static final String CLA_TEASING= "teasing";
+	  public static final String CLA_AUTHOR_ROLE= "author_role";
+	  public static final String CLA_FORM= "form";
+	  
+}
